0390684d57627490aae7d71ef93ba940b31585d2
JustinLoye <justin.loye@gmail.com>
2025-09-29 10:16:46 +0000

    add pypi cd

---
+++ tests/smoke_test.py
@@ -0,0 +1,11 @@
+import sys
+
+def test_smoke():
+    """A minimal test to ensure the package and its main components can be imported."""
+    import pybgpkitstream
+    assert pybgpkitstream is not None
+
+
+if __name__ == "__main__":
+    test_smoke()
+    sys.exit(0)
